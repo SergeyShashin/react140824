f545657863da92e52f46f91f9eb2a3547afc2da8
SergeyShashin <s9997910@yandex.ru>
2024-08-17 23:26:51 +0300

    React 140824. Lesson2. handlerClickControl done.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,34 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 export class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+
+  handlerClickControl = (e) => {
+    if (e.target.tagName !== 'BUTTON') {
+      return
+    }
+
+    let targetOperation = Number(e.target.dataset.operation);
+
+    this.setState((prevState) => ({
+      counter: prevState.counter + targetOperation
+    }));
+
   }
 
   render() {
-    return <h2>Привет из App!</h2>
+    const { counter } = this.state;
+    return (
+      <section id="app" onClick={this.handlerClickControl}>
+        <button data-operation="-1" id="minus">-</button>
+        <span id="monitor">{counter}</span>
+        <button data-operation="1" id="plus">+</button>
+      </section>
+    )
   }
 }
