b05dbcc4a89884347ba0d4645d7e3c9e9a590a3a
SergeyShashin <s9997910@yandex.ru>
2024-08-18 11:11:08 +0300

    React 140824. Lesson2. <App/> updated. <Counter/> done.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,27 @@
 import React, { Component } from "react";
+import { Counter } from "components/Counter";
 
 export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0
+      showCounter: false
     };
+
   }
 
-  handlerClickControl = (e) => {
-    if (e.target.tagName !== 'BUTTON') {
-      return
-    }
-
-    let targetOperation = Number(e.target.dataset.operation);
-
-    this.setState((prevState) => ({
-      counter: prevState.counter + targetOperation
-    }));
-
+  handleToggle = () => {
+    this.setState({ showCounter: !this.state.showCounter });
   }
 
   render() {
-    const { counter } = this.state;
     return (
-      <section id="app" onClick={this.handlerClickControl}>
-        <button data-operation="-1" id="minus">-</button>
-        <span id="monitor">{counter}</span>
-        <button data-operation="1" id="plus">+</button>
+      <section>
+        {
+          this.state.showCounter && <Counter />
+        }
+
+        <button onClick={this.handleToggle}>Togglle</button>
       </section>
     )
   }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+export class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+      interval: null
+    };
+  }
+
+  componentDidMount() {
+    this.state.interval = setInterval(() => console.log('fire'), 200);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.interval);
+  }
+
+  handlerClickControl = (e) => {
+    if (e.target.tagName !== 'BUTTON') {
+      return
+    }
+
+    let targetOperation = Number(e.target.dataset.operation);
+
+    this.setState((prevState) => ({
+      counter: prevState.counter + targetOperation
+    }));
+
+  }
+
+  render() {
+    const { counter } = this.state;
+    return (
+      <section id="app" onClick={this.handlerClickControl}>
+        <button data-operation="-1" id="minus">-</button>
+        <span id="monitor">{counter}</span>
+        <button data-operation="1" id="plus">+</button>
+      </section>
+    )
+  }
+}
