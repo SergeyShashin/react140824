30d3279d9030a36cece55af7a098523878432990
SergeyShashin <s9997910@yandex.ru>
2024-08-20 17:17:33 +0300

    React 140824. Lesson3. Updated.

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -1,28 +1,36 @@
 import './message.scss';
 import React, { Component } from "react";
 
+import classNames from 'classnames';
+
 export class Message extends Component {
   constructor(props) {
     super(props);
+
+    // const classes=classNames('message', {
+    //   author:
+    // })
   }
 
-  get align() {
+  // get align() {
 
-    if (this.props.message.author !== 'Бот') {
-      return 'start'
-    }
+  //   if (this.props.message.author !== 'Бот') {
+  //     return 'start'
+  //   }
 
-    if (this.props.message.author === 'Бот') {
-      return 'end'
-    }
+  //   if (this.props.message.author === 'Бот') {
+  //     return 'end'
+  //   }
 
-  }
+  // }
+
 
   render() {
     return (
-      <section className='message' style={{ alignSelf: `flex-${this.align}`}}>
+      // <section className='message' style={{ alignSelf: `flex-${this.align}`}}>
+      <section className='message' >
         <p>{this.props.message.text}</p>
-        <p>{this.props.message.author}</p>
+        <p className='sender'>{this.props.message.author}</p>
       </section>
     )
   }
