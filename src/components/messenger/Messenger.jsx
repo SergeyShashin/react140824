66c89234d5f620dbe58389dd4548a81865bf65e2
SergeyShashin <s9997910@yandex.ru>
2024-08-21 16:46:07 +0300

    React 140824. Lesson3. Added <Layout/>, <Header/>.

--- src/components/messenger/Messenger.jsx
+++ src/components/messenger/Messenger.jsx
@@ -8,28 +8,10 @@ import { Layout } from "components/Layout";
 export class Messenger extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      messages: [
-        // { author: 'Друг', text: 'Привет, друг!' },
-        // { author: 'Друг', text: 'Как дела?' },
-        // { author: 'Друг', text: 'Как погода?' },
-        // { author: 'Друг', text: 'Как настроение?' },
-      ],
-    };
+    
   }
 
-  handleSend = (message) => {
-    this.setState({ messages: this.state.messages.concat([message]) });
-  }
-
-  componentDidUpdate() {
-    let { author } = this.state.messages[this.state.messages.length - 1];
-    if (author !== 'Бот') {
-      setTimeout(() => this.setState({
-        messages: this.state.messages.concat({ author: 'Бот', text: `Здравствуйте, ${author}. Ваше сообщение получено.` })
-      }), 1000);
-    }
-  }
+ 
 
   render() {
     return (
