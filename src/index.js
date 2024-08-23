960c129fa670bd8dec1d6259637d97b2484a217c
SergeyShashin <s9997910@yandex.ru>
2024-08-23 08:50:00 +0300

    React 140824. Lesson 4 updated.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import 'assets/global.scss';
-import './routes.js';
 
 import React from "react";
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { App } from 'components/app';
-
+import { App } from 'components/app';
 
 
 // const element = <h2 className="element">Привет из Реакта+jsx!</h2>;
@@ -16,18 +13,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // const Messages = props => props.messages.map(message => <Message text={message} />);
 
 const root = createRoot(document.getElementById('root'));
-
-// root.render(<Messages messages={messages} />);
-
-// root.render(
-//   <BrowserRouter>
-//     <Routes>
-//       {routes.map((route, idx) => <Route key={idx} {...route} />)}
-//     </Routes>
-//   </BrowserRouter>
-// );
-
 root.render(
-<App />
-);
+  <App />
+  );
+  
+
+
+
 
